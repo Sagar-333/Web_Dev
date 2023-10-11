da36a8ade8aff65693e2d4b7c75811206f9c8b90
Sagar-333 <sagarkhanna030303@gmail.com>
2023-10-11 12:31:25 +0530

    exp_analysis

--- JavaScript/week1/assignment1/expanditure-analysis.js
+++ JavaScript/week1/assignment1/expanditure-analysis.js
@@ -5,14 +5,46 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 function analysis(transcation) {
-    var array = {};
-    
-    console.log(transcation.price)
+    var spendEst = {};
+    for (let i = 0; i < transcation.length; i++) {
+      var t = transcation[i];
+      if (spendEst[t.category]) {
+        spendEst[t.category]=spendEst[t.category]+t.price
+      }else{
+        spendEst[t.category]=t.price
+      }
+    }
+    var keys = Object.keys(spendEst);
+
+    let answer = [];
+    for (let i = 0; i < keys.length; i++) {
+      var category = keys[i];
+      var obj = {
+        category: category,
+        amountSpend: spendEst[category]
+      }
+      answer.push(obj)
+    }
+    console.log(answer);
 }
-var transcation={
-    itemName: "abc",
-    category: "xyz",
-    price: 15000,
+var transcation=[{
+    itemName: "biscuit",
+    category: "food",
+    price: 15,
     timestamp: "12:00"
-}
+},
+  {
+    itemName: "ice-cream",
+    category: "food",
+    price: 80,
+    timestamp: "12:10"
+  },
+  {
+    itemName: "juice",
+    category: "drink",
+    price: 60,
+    timestamp: "12:15"
+  },
+  
+]
 analysis(transcation);
